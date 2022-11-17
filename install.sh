bdfedc3e1d83a8a9982f6a985cb7717e023d0f2e
Glenn Cottam <35470791+GlennCottam@users.noreply.github.com>
2022-11-17 14:57:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@ cd ~
 echo "Cloning ZSH Repo"
 git clone https://github.com/GlennCottam/zshrc.git .zsh
 
-echo "Installing OH-MY-ZSH"
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 echo "Cloning ZSH Themes"
 git clone https://github.com/GlennCottam/omz-themes.git ~/.oh-my-zsh/custom/themes/omz-themes
 
